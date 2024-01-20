ab8103c05b7d99ccd0a7fe0997cb39cd09d182ca
monarxxjain <jainmonark1000@gmail.com>
2024-01-20 19:47:48 +0530

    Init Listeners

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -7,6 +7,19 @@ class SocketService {
         console.log("Init Socket Service...")
         this._io = new Server();
     }
+
+    public initListeners(){
+        const io = this.io;
+        console.log("Init Socket Listeners...");
+
+        io.on('connect', (socket) => {
+            console.log(`New Socket Connected`, socket.id);
+            socket.on('event:message', async({message}: {message: string}) => {
+                console.log("New Message Received", message);
+            })
+        })
+    }
+
     get io(){
         return this._io;
     }
