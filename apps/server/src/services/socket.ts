d748fb646c8bfcd7d1da41dbe2873ad65dd70b2a
monarxxjain <jainmonark1000@gmail.com>
2024-01-21 02:13:42 +0530

    Real Time Scaleable Chatting App Ready using - Socket.io & Redis.io

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,4 +1,18 @@
 import {Server} from 'socket.io'
+import { Redis } from 'ioredis';
+
+const pub = new Redis({
+    host: 'redis-14882.c321.us-east-1-2.ec2.cloud.redislabs.com',
+    port: 14882,
+    username: 'default',
+    password: 'ydQ7OinuFTB81DIXAap17rGo7pHSXwgi'
+});
+const sub = new Redis({
+    host: 'redis-14882.c321.us-east-1-2.ec2.cloud.redislabs.com',
+    port: 14882,
+    username: 'default',
+    password: 'ydQ7OinuFTB81DIXAap17rGo7pHSXwgi'
+})
 
 class SocketService {
     private _io: Server;
@@ -11,6 +25,7 @@ class SocketService {
                 origin: '*'
             }
         });
+        sub.subscribe('MESSAGES');
     }
 
     public initListeners(){
@@ -21,7 +36,15 @@ class SocketService {
             console.log(`New Socket Connected`, socket.id);
             socket.on('event:message', async({message}: {message: string}) => {
                 console.log("New Message Received", message);
+                // Publish this message to REDIS
+                await pub.publish('MESSAGES', JSON.stringify({ message }));
             })
+        })
+
+        sub.on('message', (channel, message) => {
+            if(channel === 'MESSAGES'){
+                io.emit('message', message)
+            }
         })
     }
 
--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -8,6 +8,7 @@ interface SocketProviderProps {
 
 interface ISocketContext {
     sendMessage: (msg: string) => any;
+    messages: string[];
 }
 
 const SocketContext = createContext<ISocketContext | null>(null);
@@ -20,7 +21,8 @@ export const useSocket = () => {
 }
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({children}) => {
-    const [socket, setSocket] = useState<Socket>()
+    const [socket, setSocket] = useState<Socket>();
+    const [messages, setMessages] = useState<string[]>([]);
 
     const sendMessage: ISocketContext['sendMessage'] = useCallback((msg) => {
         console.log("Send Message", msg);
@@ -29,22 +31,26 @@ export const SocketProvider: React.FC<SocketProviderProps> = ({children}) => {
         }
     },[socket])
 
-    const contextValue: ISocketContext = {
-        sendMessage: sendMessage,
-    };
+    const onMessageReceived = useCallback((msg: string) => {
+        console.log('Message Received from Server', msg);
+        const {message} = JSON.parse(msg) as {message: string}
+        setMessages( prev => [...prev, message] );
+    }, [])
 
     useEffect(() => {
         const _socket = io('http://localhost:8000');
+        _socket.on('message', onMessageReceived);
         setSocket(_socket);
 
         return () => {
             _socket.disconnect();
+            _socket.off('message');
             setSocket(undefined);
         }
     }, [])
 
     return(
-        <SocketContext.Provider value={contextValue}>
+        <SocketContext.Provider value={{ sendMessage, messages }}>
             {children}
         </SocketContext.Provider>
     )
